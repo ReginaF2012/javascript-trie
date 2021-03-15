c67705d7e8d8923f37ab32550230525d0776e1a9
ReginaF2012 <skritschy@gmail.com>
2021-03-14 22:43:32 -0500

    insert and search methods

--- trie.js
+++ trie.js
@@ -2,11 +2,36 @@ class Trie {
     constructor() {
         this.root = new Node();
     }
+
+    insert(word) {
+        let currNode = this.root;
+
+        for (const letter of word) {
+            if (!currNode.letters[letter]) {
+                currNode.letters[letter] = new Node();
+            }
+            currNode = currNode.letters[letter];
+        }
+
+        currNode.endOfWord = true;
+        return currNode;
+    }
+
+    search(word) {
+        let currNode = this.root;
+        for (const letter of word) {
+            if (!currNode.letters[letter]) return false;
+
+            currNode = currNode.letters[letter];
+        }
+
+        return currNode.endOfWord;
+    }
 }
 
 class Node {
     constructor() {
-        this.chars = {};
+        this.letters = {};
         this.endOfWord = false;
     }
 }
