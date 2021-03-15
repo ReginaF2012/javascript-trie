d9a764906e648b80220e5dfdfe73c40faddfe239
ReginaF2012 <skritschy@gmail.com>
2021-03-14 22:33:55 -0500

    trie and node class constructors

--- trie.js
+++ trie.js
@@ -1,11 +1,12 @@
 class Trie {
     constructor() {
-
+        this.root = new Node();
     }
 }
 
 class Node {
     constructor() {
-        
+        this.chars = {};
+        this.endOfWord = false;
     }
 }
