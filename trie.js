9adff12539f05d92ef8941ccaf6827b211e53131
ReginaF2012 <skritschy@gmail.com>
2021-03-20 19:19:08 -0500

    findAllWordsWithPrefix method complete

--- trie.js
+++ trie.js
@@ -4,38 +4,94 @@ class Trie {
     }
 
     insert(word) {
+        if (!word) return false;
+
         let currNode = this.root;
 
         for (const letter of word) {
-            if (!currNode.letters.has(letter)) {
-                currNode.letters.set(letter) = new Node();
+            if (!currNode.children.has(letter)) {
+                currNode.children.set(letter, new Node(letter));
             }
-            currNode = currNode.letters.get(letter);
+            currNode = currNode.children.get(letter);
         }
 
         currNode.endOfWord = true;
         return currNode;
     }
 
-    search(word) {
-        let currNode = this.root;
-        for (const letter of word) {
-            if (!currNode.letters.has(letter)) return false;
+    hasWord(word, start = this.root) {
+        if (!word) return false;
 
-            currNode = currNode.letters.get(letter);
+        let currNode = start;
+        for (const letter of word) {
+            if (!currNode.children.has(letter)) return false;
+
+            currNode = currNode.children.get(letter);
         }
 
         return currNode.endOfWord;
     }
 
-    remove(word) {
+    findAllWithPrefix(prefix, start = this.root) {
+        if (!prefix) return false;
+        let words = [],
+            currNode = start;
+
+        for (const letter of prefix) {
+            if (!currNode.children.has(letter)) return words;
+            currNode = currNode.children.get(letter);
+        }
+
+        currNode.children.forEach((child) =>
+            this.getWordsFrom(child, prefix, words)
+        );
+
+        return words;
+    }
+
+    getWordsFrom(node, string, array = []) {
+        if (!node || !string) return;
+
+        string += node.value;
+
+        if (node.endOfWord) array.push(string);
+
+        node.children.forEach((child) => {
+            this.getWordsFrom(child, string, array);
+        });
+    }
+
+    removeWord(word) {
 
     }
 }
 
 class Node {
-    constructor() {
-        this.letters = new Map();
+    constructor(value = "") {
+        this.children = new Map();
+        this.value = value;
         this.endOfWord = false;
     }
 }
+
+let trie = new Trie();
+trie.insert("cat");
+trie.insert("bet");
+trie.insert("be");
+trie.insert("beaver");
+trie.insert("car");
+trie.insert("canoe");
+trie.insert("cot");
+trie.insert("cranberries");
+// console.log(trie.hasWord('cat'));
+// console.log(trie.hasWord('car'));
+// console.log(trie.hasWord('be'));
+// console.log(trie.hasWord('bet'));
+// console.log(trie.hasWord('cap'));
+console.log('all words that start with "c":', trie.findAllWithPrefix("c"));
+console.log('all words that start with "ca":', trie.findAllWithPrefix("ca"));
+console.log('all words that start with "be":', trie.findAllWithPrefix("be"));
+console.log(
+    'all words that start with: "cran":',
+    trie.findAllWithPrefix("cran")
+);
